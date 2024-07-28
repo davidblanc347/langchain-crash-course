f33aba7211233ca350925597375020e51dcfc2ea
David Blanc Brioir <davidblancbrioir@gmail.com>
2024-07-28 10:37:01 +0200

    Update 1a_interrogation.py

--- 4_rag/1a_interrogation.py
+++ 4_rag/1a_interrogation.py
@@ -112,6 +112,7 @@ results = collection.query(
   query_embeddings=[response["embedding"]],
   n_results=15
 )
+datatotal = results['documents']
 color_list = [color.WARNING,color.OKGREEN,color.OKBLUE]
 index_couleur=0
 for i,data in enumerate(results['documents'][0]):
@@ -129,7 +130,7 @@ for i,data in enumerate(results['documents'][0]):
 time_debut = time.time()
 output = ollama.generate(
   model="llama3.1",
-  prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
+  prompt=f"Using this data: {datatotal}. Respond to this prompt: {prompt}"
 )
 print(time.time()-time_debut)
 print(f"{color.OKCYAN}{output['response']}{color.ENDC}")
