51b8e25ded63966849a308231316a7a1f7997ae7
David Blanc Brioir <davidblancbrioir@gmail.com>
2024-07-29 13:04:24 +0200

    v

--- 4_rag/test.py
+++ 4_rag/test.py
@@ -1,13 +1,4 @@
 
-import os
+import datetime
 
-# Define the directory containing the text file and the persistent directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "books", "moby_dick.txt")
-# Test reading the file directly
-try:
-    with open(file_path, 'r',encoding='utf-8') as test_file:
-        print("First 100 characters of the file:")
-        print(test_file.read(100))
-except Exception as e:
-    raise RuntimeError(f"Failed to read the file due to: {e}")
+print(datetime.datetime.now())
